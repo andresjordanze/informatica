1d9d24fcc6d7693323626471b9e52f2faa0fe2fb
MartinArispeRiveros <martinarisperiveros@gmail.com>
2014-07-28 19:45:53 -0400

    nose

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,12 +9,12 @@ class Product < ActiveRecord::Base
     
 
 
-    has_many :productorders, :dependent => :destroy
+    
     
     validates :name, presence: {:message => "Debe ingresar el nombre del producto"}	
 	validates :name, uniqueness: {case_sensitive: false, :message => "El nombre ya existe"}
-	#validates :name, format: { with: /\A[a-zA-Z\d\s]+\z/,
-    #message: "Solo Letras Permitidas" }
+	validates :name, format: { with: /\A[a-zA-Z\d\s]+\z/,
+    message: "Solo Letras Permitidas" }
     validates :name, length: {minimum: 5, :message => "Minimo 5 caracteres"}
     has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" },
 		:storage => :dropbox,
